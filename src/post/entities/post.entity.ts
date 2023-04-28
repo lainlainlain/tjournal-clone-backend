5b0dbdf5464c372401e1b6a9c48493b4c4c95192
layerl <layerlain@yandex.ru>
2023-04-29 04:34:10 +0900

    added route popular

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -20,6 +20,9 @@ export class PostEntity {
   @Column({ nullable: true })
   tags?: string;
 
+  @Column({ default: 0 })
+  views: number;
+
   @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date;
 
